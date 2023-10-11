9c697cb41db62a729223b420ae20c26a43c2b2a8
“neocrmoficial” <neocrmoficial@gmail.com>
2023-10-11 17:56:14 -0300

    inicio do desafio

---
+++ index.js
@@ -0,0 +1,14 @@
+class hero {
+    constructor(name, tip, atack) {
+        this.name = name,
+        this.tip = tip,
+        this.atack = atack
+    }
+}
+
+let battle =  new hero("Chico Buaque", "Mago", "usou magia");
+
+
+function atackHero(name, tip, atack) {
+    
+}
